739b9b3412cf6aeeb2f9acf187cf27ac222b3985
mauricio.carunchio.contractor <mauricio.carunchio.contractor@gdevtools.com>
2022-09-23 15:18:08 -0300

    Datos de testimonios se obtienen de un archivo aparte

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -4,10 +4,10 @@ import '../hojas-de-estilo/Testimonio.css';
 function Testimonio(props) {
   return (
     <div className="contenedor-testimonio">
-      <img 
+      <img
         className="imagen-testimonio"
         src={require(`../imagenes/testimonio-${props.imagen}.png`)}
-        alt={"Foto de " + props.nombre}/>
+        alt={"Foto de " + props.imagen[0].toUpperCase() + props.imagen.substring(1)} />
       <div className="contenedor-texto-testimonio">
         <p className="nombre-testimonio">
           <strong>{props.nombre}</strong> en {props.pais}
@@ -15,7 +15,7 @@ function Testimonio(props) {
         <p className="cargo-testimonio">
           {props.cargo} en <strong>{props.empresa}</strong>
         </p>
-        <p className="texto-testimonio">"{props.testimonio}"</p>
+        <p className="texto-testimonio" dangerouslySetInnerHTML={{ __html: props.testimonio }}></p>
       </div>
     </div>
   );
